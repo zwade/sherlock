1fe18cd286f7cef08531a1dd8fbf61ecd319bf1d
Ethan Lowman <ethan.l.527@gmail.com>
2015-05-16 19:32:26 -0400

    Change some model stuff and add string methods

--- sherlock/core/models.py
+++ sherlock/core/models.py
@@ -1,15 +1,30 @@
 from django.db import models
 from django.contrib.auth.models import User
+import random
+import string
+
+
+def random_string(N):
+    return ''.join(random.choice(string.ascii_lowercase) for _ in range(N))
 
 
 class Hunt(models.Model):
     owner = models.ForeignKey(User, related_name="hunts")
     participants = models.ManyToManyField(User)
-    description = models.CharField(max_length=1000)
+    description = models.TextField(max_length=1000)
     name = models.CharField(max_length=100)
-    photo = models.ImageField()
+    photo = models.ImageField(blank=True)
     start_time = models.DateTimeField()
     end_time = models.DateTimeField()
+    slug = models.CharField(max_length=8, unique=True, blank=True)
+
+    def save(self):
+        if not self.slug:
+            self.slug = random_string(8)
+        super(Hunt, self).save()
+
+    def __str__(self):
+        return self.name
 
 
 class Clue(models.Model):
@@ -18,6 +33,9 @@ class Clue(models.Model):
     points = models.IntegerField()
 
     hunt = models.ForeignKey(Hunt)
+
+    def __str__(self):
+        return "{} (Hunt {})".format(self.name, self.hunt.name)
 
 
 class Submission(models.Model):
@@ -30,3 +48,6 @@ class Submission(models.Model):
 
     clue = models.ForeignKey(Clue)
     user = models.ForeignKey(User)
+
+    def __str__(self):
+        return "User {} to Clue {})".format(self.user, self.clue)
