486241c2790dfd466cfa77213beb1a1ce2678763
Strikeskids <github@strikeskids.com>
2015-05-16 20:00:56 -0400

    Add auth

--- sherlock/core/forms.py
+++ sherlock/core/forms.py
@@ -1,1 +1,18 @@
 from django import forms
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+
+class AuthenticationForm(AuthenticationForm):
+
+    def is_valid(self):
+        success = super(AuthenticationForm, self).is_valid()
+
+        for field, error in self.errors.items():
+            if field != '__all__':
+                self.fields[field].widget.attrs.update({'class': 'invalid'})
+            else:
+                self.fields['password'].widget.attrs.update({'class': 'invalid'})
+
+        return success
+
+class UserCreationForm(UserCreationForm):
+    pass
--- sherlock/core/views/auth.py
+++ sherlock/core/views/auth.py
@@ -1,3 +1,48 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate
+from .forms import AuthenticationForm, UserCreationForm
 
+class login_view(View):
+    def get(self, request):
+        if request.user.is_authenticated():
+            return redirect('/')
+        return render(request, 'login.html', {'form': AuthenticationForm()})
 
+    def post(self, request):
+        form = AuthenticationForm(request.POST)
+
+        if form.is_valid():
+            login(request, form.get_user())
+
+            next_url = request.GET.get('next', '/')
+
+            return redirect(next_url)
+
+        return render(request, 'login.html', {'form': form})
+
+class register_view(View):
+    def get(self, request):
+        if request.user.is_authenticated():
+            return redirect('/')
+        return render(request, 'register.html', {'form': UserCreationForm()})
+
+    def post(self, request):
+        form = UserCreationForm(request.POST)
+
+        if form.is_valid():
+            form.save()
+
+            # Login the created user
+
+            user = authenticate(username=form.cleaned_data['username'], password=form.cleaned_data('password'))
+            login(request, user)
+
+            return redirect('/')
+
+        return render(request, 'register.html', {form: form})
+
+def index_view(request):
+    if not request.user.is_authenticated():
+        return render(request, 'index.html')
+    return render(request, 'hunts.html')
