073532bb3ff8e89ea2378bd06c0d8522263ab43c
Ethan Lowman <ethan.l.527@gmail.com>
2015-05-16 20:00:44 -0400

    Add urls

--- sherlock/core/urls.py
+++ sherlock/core/urls.py
@@ -1,5 +1,10 @@
 from django.conf.urls import url
+from .views import app
+from .views import auth
 
 urlpatterns = [
-
+    # url(r"^$", auth.index_view),
+    # url(r"^login$", auth.login_view),
+    # url(r"^logout$", auth.logout_view),
+    # url(r"^register$", auth.register_view),
 ]
