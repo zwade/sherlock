1c7ed9cf9a8dd9ac3b735af49eb88c4854d63667
Strikeskids <github@strikeskids.com>
2015-05-16 23:56:49 -0400

    lots of eggplanting

--- sherlock/core/views/game.py
+++ sherlock/core/views/game.py
@@ -1,4 +1,4 @@
-from django.contrib.auth import authenticate
+from django.shortcuts import render, redirect
 from django.views.generic import View
 from ..models import Hunt
 from ..forms.game import SubmissionForm, HuntForm, NewClueForm
@@ -6,7 +6,7 @@ from . import LoginRequiredMixin
 
 class NewHuntView(LoginRequiredMixin, View):
     def get(self, request):
-        return render('new_hunt.html', {'form': HuntForm()})
+        return render(request, 'new_hunt.html', {'form': HuntForm()})
 
     def post(self, request):
         form = HuntForm(request.POST)
@@ -18,7 +18,7 @@ class NewHuntView(LoginRequiredMixin, View):
 
             return redirect('edit_hunt', slug=newHunt.slug)
 
-        return render('new_hunt.html', {'form': form})
+        return render(request, 'new_hunt.html', {'form': form})
 
 class EditHuntView(LoginRequiredMixin, View):
     def get(self, request, slug):
@@ -39,7 +39,7 @@ class EditHuntView(LoginRequiredMixin, View):
         if hunt.owner != request.user:
             return redirect('view_hunt', slug=slug)
 
-        return render('edit_hunt.html', {'edit': HuntForm(hunt), 'clue': NewClueForm(), 'hunt': hunt, 'clues': hunt.clue_set.all()})
+        return render(request, 'edit_hunt.html', {'edit': HuntForm(hunt), 'clue': NewClueForm(), 'hunt': hunt, 'clues': hunt.clue_set.all()})
 
 class NewClueAjax(LoginRequiredMixin, View):
     def post(self, request, slug):
@@ -48,12 +48,12 @@ class NewClueAjax(LoginRequiredMixin, View):
         if form.is_valid():
             clue = form.save()
 
-            return render('clue_row.html', {'clue': clue})
+            return render(request, 'clue_row.html', {'clue': clue})
 
-        raise render('clue_form.html', {'clue': form}, status=400)
+        raise render(request, 'clue_form.html', {'clue': form}, status=400)
 
 class HuntView(View):
     def get(self, request, slug):
         hunt = Hunt.objects.get(slug=slug)
 
-        return render('hunt.html', {'hunt': hunt, 'owned': hunt.owner == request.user})
+        return render(request, 'clues.html', {'hunt': hunt, 'owned': hunt.owner == request.user})
--- sherlock/settings.py
+++ sherlock/settings.py
@@ -126,6 +126,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
+if DEBUG:
+    STATICFILE_DIRS = [
+        os.path.join(BASE_DIR, 'sherlock/core/static')
+    ]
 
 LOGGING = {
     'version': 1,
