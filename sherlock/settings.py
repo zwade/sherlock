489922bf330775ef3780bc78ee81c3e085703e16
Ethan Lowman <ethan.l.527@gmail.com>
2015-05-17 14:00:08 -0400

    Always use sqlite

--- sherlock/settings.py
+++ sherlock/settings.py
@@ -101,15 +101,12 @@ WSGI_APPLICATION = 'sherlock.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.8/ref/settings/#databases
 
-if DEBUG:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-        }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
-else:
-    raise Exception("Do this")
+}
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.8/topics/i18n/
