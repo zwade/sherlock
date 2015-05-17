10bceb71048121253269c3ef11fd52ad61d75e76
Ethan Lowman <ethan.l.527@gmail.com>
2015-05-17 12:36:58 -0400

    Merge migrations

---
+++ sherlock/core/migrations/0009_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('core', '0008_auto_20150517_1229'),
+        ('core', '0007_auto_20150517_1202'),
+    ]
+
+    operations = [
+    ]
